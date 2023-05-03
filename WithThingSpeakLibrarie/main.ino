438556e73771a9f159a6bf3ed5b29c76c56c54ca
Mateus Alves de Oliveira <132407763+MateusAlves18@users.noreply.github.com>
2023-05-03 02:06:17 +0000

    Insert Codes

---
+++ WithThingSpeakLibrarie/main.ino
@@ -0,0 +1,110 @@
+#include <DHT.h>
+#include <DHT_U.h>
+#include <WiFi.h>
+#include "ThingSpeak.h"
+
+#define DHTPIN 15   
+#define DHTTYPE    DHT22   
+#define CANAL1 25
+#define CANAL2 33
+
+DHT_Unified dht(DHTPIN, DHTTYPE);
+float temp;
+float humidity;
+
+WiFiClient client;
+
+unsigned long myChannelNumber =  1000000;
+const char * myWriteAPIKey = "SECRET";
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+  sensor_t sensor;
+  dht.temperature().getSensor(&sensor);
+  Serial.println(F("------------------------------------"));
+  Serial.println(F("Temperature Sensor"));
+  Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
+  Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
+  Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
+  Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("°C"));
+  Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("°C"));
+  Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("°C"));
+  Serial.println(F("------------------------------------"));
+  dht.humidity().getSensor(&sensor);
+  Serial.println(F("Humidity Sensor"));
+  Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
+  Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
+  Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
+  Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("%"));
+  Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("%"));
+  Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("%"));
+  Serial.println(F("------------------------------------"));
+  Serial.println("Conectando-se ao Wi-Fi");
+  WiFi.begin("Wokwi-GUEST", "", 6);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(100);
+    Serial.print(".");
+  }
+  Serial.println("\n Conectado!");
+  Serial.println(F("------------------------------------"));
+  WiFi.mode(WIFI_STA); 
+  ThingSpeak.begin(client);
+  pinMode(CANAL1, OUTPUT);
+  pinMode(CANAL2, OUTPUT);
+}
+
+void loop() {
+  sensors_event_t event;
+  dht.temperature().getEvent(&event);
+  if (isnan(event.temperature)) {
+    Serial.println(F("Error reading temperature!"));
+  }
+  else {
+    temp = event.temperature;
+    Serial.print(F("Temperature: "));
+    Serial.print(temp);
+    Serial.println(F("°C"));
+    if(temp > 35 ){
+      digitalWrite(CANAL1, HIGH);
+    }else{
+      digitalWrite(CANAL1, LOW);  
+    }
+    int x = ThingSpeak.writeField(myChannelNumber, 1, temp, myWriteAPIKey);
+    if(x == 200){
+      Serial.println("Channel 1 (temperature) update successful.");
+    }
+    else{
+      Serial.println("Problem updating channel 1 (temperature). HTTP error code " + String(x));
+    }           
+  }
+  Serial.println(F("------------------------------------"));
+  delay(15000);
+
+  dht.humidity().getEvent(&event);
+  if (isnan(event.relative_humidity)) {
+    Serial.println(F("Error reading humidity!"));
+  }
+  else {
+    humidity = event.relative_humidity;
+    Serial.print(F("Humidity: "));
+    Serial.print(humidity);
+    Serial.println(F("%"));
+    if(humidity > 70){
+      digitalWrite(CANAL2, HIGH);
+    } else{
+      digitalWrite(CANAL2, LOW);
+    }
+    int y = ThingSpeak.writeField(myChannelNumber, 2, humidity, myWriteAPIKey);
+
+    if(y == 200){
+      Serial.println("Channel 2 (humidity) update successful.");
+    }
+    else{
+      Serial.println("Problem updating channel 2 (humidity). HTTP error code " + String(y));
+    }
+  }
+  Serial.println(F("------------------------------------"));
+  delay(15000);
+
+}
---
+++ WithWifiClient/main.ino
@@ -0,0 +1,134 @@
+#include <DHT.h>
+#include <DHT_U.h>
+#include <WiFi.h>
+
+#define DHTPIN 15   
+#define DHTTYPE    DHT22   
+#define CANAL1 25
+#define CANAL2 33
+
+DHT_Unified dht(DHTPIN, DHTTYPE);
+float temp;
+float humidity;
+
+
+char server[] = "api.thingspeak.com";
+String writeAPIKey = "SECRET";
+
+unsigned long lastConnectionTime = 0;
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+  sensor_t sensor;
+  dht.temperature().getSensor(&sensor);
+  Serial.println(F("------------------------------------"));
+  Serial.println(F("Temperature Sensor"));
+  Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
+  Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
+  Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
+  Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("°C"));
+  Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("°C"));
+  Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("°C"));
+  Serial.println(F("------------------------------------"));
+  dht.humidity().getSensor(&sensor);
+  Serial.println(F("Humidity Sensor"));
+  Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
+  Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
+  Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
+  Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("%"));
+  Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("%"));
+  Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("%"));
+  Serial.println(F("------------------------------------"));
+  Serial.println("Conectando-se ao Wi-Fi");
+  WiFi.begin("Wokwi-GUEST", "", 6);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(100);
+    Serial.print(".");
+  }
+  Serial.println("\n Conectado!");
+  Serial.println(F("------------------------------------"));
+  WiFi.mode(WIFI_STA);
+  pinMode(CANAL1, OUTPUT);
+  pinMode(CANAL2, OUTPUT);
+}
+
+void loop(){
+  sensors_event_t event;
+  dht.temperature().getEvent(&event);
+  if (isnan(event.temperature)) {
+    Serial.println(F("Error reading temperature!"));
+  }
+  else {
+    temp = event.temperature;
+    Serial.print(F("Temperature: "));
+    Serial.print(temp);
+    Serial.println(F("°C"));
+    if(temp > 35 ){
+      digitalWrite(CANAL1, HIGH);
+    }else{
+      digitalWrite(CANAL1, LOW);  
+    }
+  }
+  Serial.println(F("------------------------------------"));
+
+
+  dht.humidity().getEvent(&event);
+  if (isnan(event.relative_humidity)) {
+    Serial.println(F("Error reading humidity!"));
+  }
+  else {
+    humidity = event.relative_humidity;
+    Serial.print(F("Humidity: "));
+    Serial.print(humidity);
+    Serial.println(F("%"));
+    if(humidity > 70){
+      digitalWrite(CANAL2, HIGH);
+    } else{
+      digitalWrite(CANAL2, LOW);
+    }
+  }
+
+  httpRequest(temp,humidity);
+  delay(5000);
+  }
+
+  void httpRequest(float field1Data, float field2Data) {
+
+    WiFiClient client;
+    
+    Serial.println(F("------------------------------------"));
+    
+    if (!client.connect(server, 80)){
+      
+        Serial.println("A conexão com o ThingSpeak Falhou");
+        lastConnectionTime = millis();
+        client.stop();
+        return;     
+    }
+    
+    else{
+    
+        String data = "field1=" + String(field1Data) + "&field2=" + String(field2Data);
+        
+        if (client.connect(server, 80)) {
+          
+            client.println("POST /update HTTP/1.1");
+            client.println("Host: api.thingspeak.com");
+            client.println("Connection: close");
+            client.println("User-Agent: ESP32WiFi/1.1");
+            client.println("X-THINGSPEAKAPIKEY: "+writeAPIKey);
+            client.println("Content-Type: application/x-www-form-urlencoded");
+            client.print("Content-Length: ");
+            client.print(data.length());
+            client.print("\n\n");
+            client.print(data);
+            lastConnectionTime = millis();   
+            delay(250);
+        }
+    }
+    Serial.println("A conexão com o ThingSpeak Funcionou, os dados foram postados!");
+    Serial.println(F("------------------------------------"));
+
+    client.stop();
+}
